b644a1fdeb07c229dc6d12993aa3efdfbc20af21
Misael <misael19972008@hotmail.com>
2022-10-07 18:11:12 -0300

    Upload dos arquivos

---
+++ concorrete_advice.py
@@ -0,0 +1,32 @@
+import csv
+import requests
+import time
+import statistics
+import threading
+url = "https://api.adviceslip.com/advice"
+
+qtd_requisi=[300]
+
+def requisicoes():
+		response = requests.request("GET", url)
+		json=response.json()
+		print(json['slip']['advice'])	
+
+for i in qtd_requisi:
+	f = open('requisicoes_concorrente'+str(i)+'.csv', 'w', newline='', encoding='utf-8')
+	w = csv.writer(f)
+	time_vec=[]
+	for j in range(0,20):
+		for k in range(0,i):
+			inicio = time.time()
+			threading.Thread(target=requisicoes).start()
+			fim = time.time()	
+			time_vec.append((fim-inicio)/100)
+			
+		print(time_vec)
+		time.sleep(30)
+		w.writerow(time_vec)
+		time_vec=[]
+	
+	f.close()
+print("Finalizei qtd_requisicoes!"+str(i))	
---
+++ sequencial_advice.py
@@ -0,0 +1,28 @@
+import csv
+import requests
+import time
+import statistics
+import numpy as np
+url = "https://api.adviceslip.com/advice"
+
+qtd_requisi=[300]
+
+
+for i in qtd_requisi:
+	f = open('requisicoes_sequencial'+str(i)+'.csv', 'w', newline='', encoding='utf-8')
+	w = csv.writer(f)
+	time_vec=[]
+	for j in range(0,20):
+		for k in range(0,i):
+			print(str(j)+' de 20 de '+str(i)+"requisicoes")
+			inicio = time.time()
+			response = requests.request("GET", url)
+			fim = time.time()	
+			time_vec.append((fim-inicio)/100)
+			json=response.json()
+			print(json['slip']['advice'])	
+			print(time_vec)
+		w.writerow(time_vec)
+		time_vec=[]
+	f.close()
+print("Finalizei qtd_requisicoes!"+str(i))
